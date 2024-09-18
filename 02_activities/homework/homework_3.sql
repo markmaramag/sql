748271ede123cf3eeb3c0ec62079239502138380
Mark Maramag <mark.maramag@gmail.com>
2024-09-17 23:34:11 -0400

    Answered all questions for homework_3.sql

--- 02_activities/homework/homework_3.sql
+++ 02_activities/homework/homework_3.sql
@@ -72,9 +72,18 @@ FROM temp.new_vendor;
 HINT: you might need to search for strfrtime modifers sqlite on the web to know what the modifers for month 
 and year are! */
 
+SELECT 
+    customer_id, 
+    strftime('%m', market_date) AS month, 
+    strftime('%Y', market_date) AS year
+FROM 
+    customer_purchases;
+
 /* 2. Using the previous query as a base, determine how much money each customer spent in April 2022. 
 Remember that money spent is quantity*cost_to_customer_per_qty. 
 
 HINTS: you will need to AGGREGATE, GROUP BY, and filter...
 but remember, STRFTIME returns a STRING for your WHERE statement!! */
+
+
 
